de78dff66fef404041ca2d792670e55b0bd35f6e
AhmedEmad101 <emada2826@gmail.com>
2025-10-29 00:27:23 +0300

    creating login request class and use it instead of the default request class

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,18 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Actions\Auth\loginAction;
-
+use App\Http\Requests\LoginRequest;
 class AuthController extends Controller
 {
-    public function login(Request $request, LoginAction $loginAction)
+    public function login(LoginRequest $request, LoginAction $loginAction)
     {
-        $data = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required', 'string'],
-        ]);
-
+        $data = $request->validated();
         $token = $loginAction->execute($data['email'], $data['password']);
-
         return response()->json([
             'message' => 'Login successful!',
             'token' => $token,
